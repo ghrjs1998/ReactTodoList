362ad3dc2a14d3f8ad19ef38a34c84f5e9735624
ghrjs1998 <park@baghogeons-MacBook-Air.local>
2023-01-19 19:08:08 +0900

    ui구현

---
+++ todolist/src/App.js
@@ -0,0 +1,28 @@
+import React from "react";
+import { createGlobalStyle } from "styled-components";
+import TodoCreate from "./components/TodoCreate";
+import TodoHead from "./components/TodoHead";
+import TodoList from "./components/TodoList";
+import TodoTemplate from "./components/TodoTemplate";
+
+// 페이지에 회색 배경색상 적용
+const GlobalStyle = createGlobalStyle`
+  body {
+    background-color: #e9ecef;
+  }
+`;
+
+function App() {
+  return (
+    <>
+      <GlobalStyle />
+      <TodoTemplate>
+        <TodoHead />
+        <TodoList />
+        <TodoCreate />
+      </TodoTemplate>
+    </>
+  );
+}
+
+export default App;
---
+++ todolist/src/components/TodoList.js
@@ -0,0 +1,24 @@
+// 할 일에 대한 정보가 들어있는 todos 배열을 내장함수 map을 사용해 여러개의 TodoItem컴포넌트를 렌더링해주는 컴포넌트
+import React from "react";
+import styled from "styled-components";
+import TodoItem from "./TodoItem";
+
+const TodoListBlock = styled.div`
+  flex: 1;
+  padding: 20px 32px;
+  padding-bottom: 48px;
+  overflow-y: auto;
+`;
+
+function TodoList() {
+  return (
+    <TodoListBlock>
+      <TodoItem text="프로젝트 생성하기" done={true} />
+      <TodoItem text="컴포넌트 스타일링 하기" done={true} />
+      <TodoItem text="Context 만들기" done={false} />
+      <TodoItem text="기능 구현하기" done={false} />
+    </TodoListBlock>
+  );
+}
+
+export default TodoList;
